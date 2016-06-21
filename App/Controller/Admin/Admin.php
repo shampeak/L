5917ba2484a9550718ee69f8e99a57b71983a752
shampeak <976107540@qq.com>
2016-06-21 19:50:05 +0800

    0.0002

---
+++ App/Controller/Admin/Admin.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+
+
+class Admin extends BaseController {
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function doIndex()
+    {
+        view('',[
+            'res'=> $res
+        ]);
+    }
+
+}
